eb2977a18abaf37c52b34ed113e1678499b0ac1f
TheInsideRip <theinsiderip@gmail.com>
2026-04-17 19:23:00 -0400

    cleanup: remove non-dashboard files, add .gitignore

--- patch2.py
+++
@@ -1,9 +0,0 @@
-h = open('index.html').read()
-old = 'if(data.overview)renderOverview(data);'
-new = 'try{if(data.overview)renderOverview(data);}catch(e){console.log("overview error",e);}'
-if old in h:
-    h = h.replace(old, new)
-    open('index.html', 'w').write(h)
-    print('patched')
-else:
-    print('ERROR: string not found in index.html')
--- patch_fmt.py
+++
@@ -1,8 +0,0 @@
-import re
-
-h = open('index.html').read()
-old = 'function fmt(v) { return v > 0 ?'
-new = 'function fmt(v) { if(typeof v==="string") return v; return v > 0 ?'
-h = h.replace(old, new)
-open('index.html', 'w').write(h)
-print('patched fmt() to handle strings')
